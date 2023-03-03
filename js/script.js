d9aa468fe0aa738e357e1b3836bf1411a13bec0c
Humayun1318 <humayunKabir6267@gmail.com>
2023-03-03 22:56:52 +0600

    without short date btn function all is done

--- js/script.js
+++ js/script.js
@@ -51,12 +51,8 @@ const displayItems = (items) => {
       <hr class="border-b border-b-2 border-gray-400 my-4" >
         <div class="card-actions flex justify-between">
           <div>
-            <h2 class=" text-[#111111] font-semibold text-xl mb-2">${
-              item.name
-            }</h2>
-            <p class="text-[#585858]"><i class="fa-solid fa-calendar-days mr-2"></i>${
-              item.published_in
-            } 
+            <h2 class=" text-[#111111] font-semibold text-xl mb-2">${item.name}</h2>
+            <p class="text-[#585858]"><i class="fa-solid fa-calendar-days mr-2"></i>${item.published_in} 
              </p>
           </div>
           
@@ -87,48 +83,86 @@ function toggleModal(modalID) {
   document.getElementById(modalID).classList.toggle("flex");
   document.getElementById(modalID + "-backdrop").classList.toggle("flex");
 }
-const displayItemDetails = (item ) => {
+const displayItemDetails = (item) => {
   const modalTitle = document.getElementById("modal-title");
   modalTitle.innerText = item?.tool_name;
   const detailsDiv = document.getElementById("details");
   detailsDiv.innerHTML = `
    <div class="bg-blue-400 p-4 md:w-[50%]" style="background: rgba(235, 87, 87, 0.05);
       border: 1px solid #EB5757;border-radius: 16px;" id="modal-id-1">
-        <p>${item?.description}</p>
+        <p class="text-lg text-[#111111] font-semibold">${
+          item?.description ? item.description : "No description"
+        }</p>
         <div class="md:flex md:flex-row gap-2  my-4 ">
             <p class="p-2 mb-2  text-[#03A30A] font-bold text-sm text-center" style="background: #FFFFFF;
               border-radius: 16px;">
-                      <span>${item?.pricing ? item.pricing[0]?.price : 'No pricing'}</span><br> 
-                      <span>${item?.pricing ? item.pricing[0]?.plan : 'No Plan'}</span></p>
+                      <span>${
+                        item?.pricing ? item.pricing[0]?.price : "No pricing"
+                      }</span><br> 
+                      <span>${
+                        item?.pricing ? item.pricing[0]?.plan : "No Plan"
+                      }</span></p>
                     <p class=" mb-2 p-2  text-[#F28927] font-bold text-sm text-center" style="background: #FFFFFF;
                     border-radius: 16px;">
-                      <span>${item?.pricing ? item.pricing[1].price : 'No pricing'}</span><br>
-                      <span>${item?.pricing ? item.pricing[1].plan : 'No plan'}</span></p>
+                      <span>${
+                        item?.pricing ? item.pricing[1].price : "No pricing"
+                      }</span><br>
+                      <span>${
+                        item?.pricing ? item.pricing[1].plan : "No plan"
+                      }</span></p>
                     <p class="p-2 text-[#EB5757] font-bold text-sm text-center" style="background: #FFFFFF;border-radius: 16px;">
-                      <span>${item?.pricing ? item.pricing[2].price : 'No pricing'}</span><br>
-                      <span>${item?.pricing ? item.pricing[2].plan : 'No plan'}</span></p>
+                      <span>${
+                        item?.pricing ? item.pricing[2].price : "No pricing"
+                      }</span><br>
+                      <span>${
+                        item?.pricing ? item.pricing[2].plan : "No plan"
+                      }</span></p>
                     </div>
                     <div class="md:grid md:grid-cols-2 w-full  gap-4">
                       <div class="">
                         <h4 class="font-semibold text-base text-[#111111]">Feature</h4>
                         <ul class="list-disc mt-3  pl-8">
                           <li class="font-normal text-sm text-[#585858]">
-                          ${item?.features ? item?.features[1]?.feature_name :'No Feature'}</li>
+                          ${
+                            item?.features
+                              ? item?.features[1]?.feature_name
+                              : "No Feature"
+                          }</li>
                           <li class="font-normal text-sm text-[#585858]">
-                          ${item?.features? item?.features[2]?.feature_name : 'No feature'}</li>
+                          ${
+                            item?.features
+                              ? item?.features[2]?.feature_name
+                              : "No feature"
+                          }</li>
                           <li class="font-normal text-sm text-[#585858]">
-                          ${item?.features? item?.features[3]?.feature_name : 'No feature'}</li>
+                          ${
+                            item?.features
+                              ? item?.features[3]?.feature_name
+                              : "No feature"
+                          }</li>
                         </ul>
                       </div>
                       <div class=" ">
                         <h4 class="font-semibold text-base text-[#111111]">Integration</h4>
                         <ul class="list-disc mt-3 pl-8">
                           <li class="font-normal text-sm text-[#585858]">
-                          ${item?.integrations ? item.integrations[0] : 'No Integrations'}</li>
+                          ${
+                            item?.integrations
+                              ? item.integrations[0]
+                              : "No Integrations"
+                          }</li>
                           <li class="font-normal text-sm text-[#585858]">
-                          ${item?.integrations ? item.integrations[1] : 'No Integrations'}</li>
+                          ${
+                            item?.integrations
+                              ? item.integrations[1]
+                              : "No Integrations"
+                          }</li>
                           <li class="font-normal text-sm text-[#585858]">
-                          ${item?.integrations ? item.integrations[2] : 'No Integration'}</li>
+                          ${
+                            item?.integrations
+                              ? item.integrations[2]
+                              : "No Integration"
+                          }</li>
                         </ul>
                       </div>
                     </div>
@@ -140,16 +174,26 @@ const displayItemDetails = (item ) => {
               <img class="rounded w-full" src="${item?.image_link[0]}" alt="">
              <div style="position: absolute; top: 12%; right: 2%; ">
                 <span class="p-3 rounded"  style="color: white; background-color: #EB5757; font-weight: bold;">
-                ${item?.accuracy? item?.accuracy?.score : 'No score'}% accuracy</span>
+                ${
+                  item?.accuracy.score ? item.accuracy.score : "No score"
+                }% accuracy</span>
              </div>
            </div>
         </div>
         <h3 class="mt-4 mb-4 text-[#111111] text-center text-base font-semibold">
-        ${item?.input_output_examples? item.input_output_examples[0].input: 'No input'}</h3>
+        ${
+          item?.input_output_examples
+            ? item.input_output_examples[0].input
+            : "No input"
+        }</h3>
         <p class="text-center  text-xs text-[#585858]  font-normal">
-        ${item?.input_output_examples? item.input_output_examples[0].output : 'No output'}</p>
+        ${
+          item?.input_output_examples
+            ? item.input_output_examples[0].output
+            : "No output"
+        }</p>
    </div>
 
   `;
 };
-loadItems(id="01");
+loadItems((id = "01"));
